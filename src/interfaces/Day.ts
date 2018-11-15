4a00b8d3b3070a5c574b7267f7d8da81314778d7
e188793k <corentin.metrot@etu.univ-nantes.fr>
2018-11-15 17:45:54 +0100

    Ajout couleurs

--- src/interfaces/Day.ts
+++ src/interfaces/Day.ts
@@ -2,6 +2,8 @@ export class Day {
     date: string;
     day_short: string;
     day_long: string;
+    icon: string;
+    icon_big: string;
     tmin: number;
     tmax: number;
     condition: string;
