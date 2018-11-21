d280cd11bfbd7474034ed8c946035555fbc85ad2
CMetrot <corentinmetrot44@gmail.com>
2018-11-21 21:06:38 +0100

    Finalisation de l'application

--- src/pages/accueil/accueil.ts
+++ src/pages/accueil/accueil.ts
@@ -4,6 +4,7 @@ import { ApiMeteoProvider } from '../../providers/api-meteo/api-meteo';
 import { Day } from '../../interfaces/Day';
 import { DayInfo } from '../../interfaces/DayInfo';
 import { City } from '../../interfaces/city';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 /**
  * Generated class for the AccueilPage page.
@@ -22,16 +23,48 @@ export class AccueilPage {
   dayInfo: DayInfo[]=[];
   day: Day;
   meteo = [];
-  i: number;
-  index: string;
+  index: Number;
+  private formulaire : FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private apiMeteo: ApiMeteoProvider ) {
-  }
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private apiMeteo: ApiMeteoProvider,
+    private formBuilder: FormBuilder) {
 
-  ionViewDidLoad() {
+      this.formulaire = this.formBuilder.group({
+        jour: ['', Validators.required]
+      });
+    }
+
+  ionViewDidLoad(a) {
+    this.dayInfo = [];
     this.apiMeteo.getMeteo().subscribe(data =>{
       this.meteo = data;
-      this.day = data.fcst_day_0;
+      if(a == null || a == 0 || !a) {
+        this.day = data.fcst_day_0;
+        this.index = 0;
+      }
+      if(a == "Mercredi") {
+        this.day = data.fcst_day_0;
+        this.index = 0;
+      }
+      if(a == "Jeudi") {
+        this.day = data.fcst_day_1;
+        this.index = 1;
+      }
+      if(a == "Vendredi") {
+        this.day = data.fcst_day_2;
+        this.index = 2;
+      }
+      if(a == "Samedi") {
+        this.day = data.fcst_day_3;
+        this.index = 3;
+      }
+      if(a == "Dimanche") {
+        this.day = data.fcst_day_4;
+        this.index = 4;
+      }
       this.dayInfo.push(data.fcst_day_0);
       this.dayInfo.push(data.fcst_day_1);
       this.dayInfo.push(data.fcst_day_2);
@@ -42,7 +75,8 @@ export class AccueilPage {
   }
 
   onSearch(){
-    
+    let jour = this.formulaire.value['jour'];
+    this.ionViewDidLoad(jour);
   }
 
 }
